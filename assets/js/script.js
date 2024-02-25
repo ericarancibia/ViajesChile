7dcdfd177f9b5e24ab06eb3158b13b032a694e4a
Eric <es.arancibia@gmail.com>
2024-02-25 02:06:19 -0300

    Proyecto Finalizado; se agrega Footer con íconos RRSS, se terminan de estilizar cards sección Destacados, se agregan funciones JQuery y JavaScript, se documenta código y recursos utilizados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,31 @@
-// Funcion Carousel
-$(document).ready(function() {
-    $('.carousel').carousel({
-      interval: 8000,
-      pause: "false"
-    });
+// Componente carousel Bootstrap en imágenes Header
+$(document).ready(function () {
+  $('.carousel').carousel({
+    interval: 8000,
+    pause: "false"
   });
+});
+
+// Componente para personalizar el color (black) de fondo del navbar al hacer scroll y remover al regresar a inicio
+$(document).scroll(function () {
+  const y = $("html").scrollTop();
+  if (y > 850) $('.navbar').addClass("bg-black")
+  else $('.navbar').removeClass("bg-black")
+});
+
+// Componente cambiar velocidad del scroll al ir a un enlace dentro de página
+var $root = $('html, body');
+$("a").click(function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    var href = this.hash;
+    $root.animate({
+      scrollTop: $(href).offset().top
+    }, 1000);
+  }
+});
+
+// Evento "click" JQuery en botón Enviar en Formulario
+$("#btn-enviar").on('click', function () {
+  alert("El Mensaje fue enviado correctamente");
+});
